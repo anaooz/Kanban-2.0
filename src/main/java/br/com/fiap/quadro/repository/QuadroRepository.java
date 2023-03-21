0c9bd3d82574e0c34e6a5ab9c7897141ee3c9e47
Mateus <mateusmarchetti.vieira@gmail.com>
2023-03-21 09:46:19 -0300

    adição do repositório

---
+++ src/main/java/br/com/fiap/quadro/repository/QuadroRepository.java
@@ -0,0 +1,9 @@
+package br.com.fiap.quadro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.quadro.models.Quadro;
+
+public interface QuadroRepository extends JpaRepository<Quadro, Long>{
+    
+}
